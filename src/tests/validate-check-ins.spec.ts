8f3b29a61aff9d3900e083e1c6133f12965e1328
Giovanna Leme Amadeu <80579735+giigio@users.noreply.github.com>
2025-09-27 17:09:52 -0300

    feat(check-ins): add LateCheckInValidationError and implement validation logic in ValidateCheckInService; update tests to cover late validation scenario

--- src/tests/validate-check-ins.spec.ts
+++ src/tests/validate-check-ins.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { LateCheckInValidationError } from '@/services/errors/late-check-in-validation-error'
 import { ResourceNotFoundError } from '@/services/errors/resource-not-found-error'
 import { ValidateCheckInService } from '@/services/validate-check-in'
 
@@ -14,7 +15,7 @@ describe('Check-in Use Case', () => {
   })
 
   afterEach(() => {
-    vi.useRealTimers()
+    vi.useFakeTimers()
   })
 
   it('should be able to validate the check in', async () => {
@@ -35,5 +36,21 @@ describe('Check-in Use Case', () => {
     await expect(() =>
       sut.execute({ checkInId: 'inexistent-id' }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await inMemoryCheckInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({ checkInId: createdCheckIn.id }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
